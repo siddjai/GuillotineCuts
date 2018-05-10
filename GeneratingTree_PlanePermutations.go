f7401f0561ee57c911d2875564dc15459669b3d4
Siddhartha Jain <siddjai21@gmail.com>
2018-05-10 21:46:23 +0530

    Set implementation remaining

--- GeneratingTree_PlanePermutations.go
+++ GeneratingTree_PlanePermutations.go
@@ -7,13 +7,13 @@ package main
 
 import "fmt"
 
-// struct to mimick set in Python3
+// struct to mimick set in Python
 // code from https://play.golang.org/p/_FvECoFvhq
 type SliceSet struct {
 	set map[[]int]bool
 }
 
-func NewSliceSet() *IntSet {
+func NewSliceSet() *SliceSet {
 	return &SliceSet{make(map[[]int]bool)}
 }
 
@@ -29,32 +29,31 @@ func min (a, b int) (int) {
 	return b
 }
 
-func localExp (perm []int, a int, c chan int) ([]int) {
+func localExp (perm []int, a int, c chan []int) {
 	// Local expansion as described in the paper
 	newPerm := make([]int, len(perm)+1)
 	for i, k := range perm {
 		if k < a {
 			newPerm[i] = k
-		}
-		else {
+		} else {
 			newPerm[i] = k+1
 		}
 	}
-	newPerm[-1] = a
+	newPerm[len(perm)] = a
 	c <- newPerm
 }
 
 func isPlane (perm []int) (bool) {
-	n = len(perm)
-	steps = make([]int, 0, n)
+	n := len(perm)
+	steps := make([]int, 0, n)
 	for k:=0; k<n-1; k++ {
-		if perm[k] < perm[k+1] - 1: steps.append(steps, k)
+		if perm[k] < perm[k+1] - 1 {steps = append(steps, k)}
 	}
 
 	for _,s := range steps {
-		m, M = perm[s], perm[s+1]
-		two, three = 1000, 0
-		prefix, suffix = perm[:s], perm[s+2:]
+		m, M := perm[s], perm[s+1]
+		two, three := 1000, 0
+		prefix, suffix := perm[:s], perm[s+2:]
 		for _,k := range prefix {
 			if (k > m) && (k < M - 1) {
 				two = min(k, two)
@@ -81,9 +80,9 @@ func main() {
 	curLevel.Add([]int{3,1,2})
 	curLevel.Add([]int{2,3,1})
 	curLevel.Add([]int{3,2,1})
-	level = 3
+	level := 3
 	for level < 20 {
-		newLevel = NewSliceSet()
+		newLevel := NewSliceSet()
 		for _,perm := range curLevel {
 			ch := make(chan int)
 			for a:=1; a<=level+1; a++ {
