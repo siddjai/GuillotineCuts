1d53513e97dd0b77bd82a82078fc69e4d89b6d99
Siddhartha Jain <siddjai21@gmail.com>
2018-06-21 10:58:27 +0530

    while implemented

--- ref/BaxterPermutation2MosaicFloorplan.py
+++ ref/BaxterPermutation2MosaicFloorplan.py
@@ -26,7 +26,22 @@ for k in range(1, n):
 		rects[perm[k]] = (oldrect[0], oldrect[1], middle, oldrect[3])
 		rects[prevlabel] = (oldrect[0], oldrect[1], oldrect[2], middle)
 		below[perm[k]] = prevlabel
-		# Implement while
+
+		while left[perm[k]] and left[perm[k]] < perm[k]:
+			l = left[perm[k]]
+			leftrect = rects[l]
+
+			rp = list(rects[perm[k]])
+			rp[0] = leftrect[0]
+			rects[perm[k]] = tuple(rp)
+			# rects[perm[k]] = (leftrect[0], rects[perm[k]][1], rects[perm[k]][2], rects[perm[k]][3])
+			rl = list(rects[l])
+			rl[3] = rp[2]
+			rects[l] = tuple(rl)
+			# rects[l] = (leftrect[0], leftrect[1], leftrect[2], rects[perm[k]][2])
+			if l in left.keys(): left[perm[k]] = left[l]
+			else: del left[perm[k]]
+
 		prevlabel = perm[k]
 	else:
 		oldrect = rects[prevlabel]
@@ -34,7 +49,22 @@ for k in range(1, n):
 		rects[perm[k]] = (middle, oldrect[1], oldrect[2], oldrect[3])
 		rects[prevlabel] = (oldrect[0], middle, oldrect[2], oldrect[3])
 		left[perm[k]] = prevlabel
-		# Implement while
+		
+		while below[perm[k]] and below[perm[k]] < perm[k]:
+			b = below[perm[k]]
+			belowrect = rects[b]
+
+			rp = list(rects[perm[k]])
+			rp[2] = belowrect[2]
+			rects[perm[k]] = tuple(rp)
+
+			rb = list(rects[b])
+			rb[1] = rp[0]
+			rects[b] = tuple(rb)
+
+			if b in below.keys(): below[perm[k]] = below[b]
+			else: del below[perm[k]]
+
 		prevlabel = perm[k]
 
 rects = set(rects.values())
