3b658e06fb5c817437a691949afebfba3cfdc7b0
alm818 <alm818@nyu.edu>
2018-07-02 17:44:48 +0400

    Improve interface of CheckSeparable

--- CheckSeperable/main.go
+++ CheckSeperable/main.go
@@ -5,32 +5,34 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-func min (a int, b int) (int) {
-	if a<b {
-		return a
-	} 
-	return b
-}
-
-func max (a int, b int) (int) {
-	if a>b {
+func min(a int, b int) int {
+	if a < b {
 		return a
 	}
 	return b
 }
 
-func addRange(stack *[][2]int, r [2]int) (){
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func addRange(stack *[][2]int, r [2]int) {
 	s := *stack
 	n := len(s)
-	if n==0 {
+	if n == 0 {
 		*stack = append(s, r)
 		return
 	}
-	
+
 	top := s[n-1]
-	if r[0]>top[1]+1 || top[0]>r[1]+1 {
+	if r[0] > top[1]+1 || top[0] > r[1]+1 {
 		*stack = append(s, r)
 		return
 	} else {
@@ -40,24 +42,31 @@ func addRange(stack *[][2]int, r [2]int) (){
 	}
 }
 
-func main(){
-	var n int
-	fmt.Scanf("%d\n", &n)
-	perm := make([]int, n)
-	for i := 0; i < n; i++ {
-      fmt.Scan(&perm[i])
-    }
+func IsSeparable(perm []int) bool {
+	var stack [][2]int
+	for _, p := range perm {
+		r := [2]int{p, p}
+		addRange(&stack, r)
+	}
+	return len(stack) == 1
+}
 
-    var stack [][2]int
-    for _, p := range perm {
-    	r := [2]int{p, p}
-    	addRange(&stack, r)
-    }
+func main() {
+	// Online
+	// var n int
+	// fmt.Scanf("%d\n", &n)
+	// perm := make([]int, n)
+	// for i := 0; i < n; i++ {
+	// 	fmt.Scan(&perm[i])
+	// }
 
-    if len(stack)==1 {
-    	fmt.Println("true")
-    } else {
-    	fmt.Println("false")
-    }
+	// Offline
+	permStr := "3 2 1 4 13 12 7 8 11 10 9 6 5"
+	tokens := strings.Split(permStr, " ")
+	perm := make([]int, len(tokens))
+	for i := 0; i < len(tokens); i++ {
+		perm[i], _ = strconv.Atoi(tokens[i])
+	}
 
+	fmt.Println(IsSeparable(perm))
 }
